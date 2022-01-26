f05e696b3ad7fdd029b3f63a2d3e351d7d122da6
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-01-26 15:32:30 +0530

    basic prototype for posts and comments ready

--- reddit_clone_proj/reddit_app/urls.py
+++ reddit_clone_proj/reddit_app/urls.py
@@ -5,5 +5,7 @@ app_name = "reddit_app"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create-post/", views.create_post, name="create_post")
+    path("create-post/", views.create_post, name="create_post"),
+    path("trending/", views.create_post, name="trending"),
+    path("post/<int:post_id>/", views.single_post, name="single_post")
 ]
--- reddit_clone_proj/reddit_app/views.py
+++ reddit_clone_proj/reddit_app/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.contrib.auth.models import User
 
 def index(request):
-    
-    return render(request, "reddit_app/index.html", {})
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, "reddit_app/index.html", {'posts':posts})
 
 def create_post(request):
-    # user_obj = User.objects.get(pk=1)
+    user_obj = User.objects.get(pk=1)
     # post_obj = Post.objects.create(
-    #                 title="Your Fav Movie?",
-    #                 text="Please comment your favourite movie watched preferably in Action/Drama genre",
+    #                 title="Rainy?",
+    #                 text="Is it raining currently in th place you live at?",
     #                 author = user_obj
     #             )
     # print(post_obj.id)
+    # print(Post.objects.all()[0].text)
     # print(Post.objects.all())
+    # post = Post.objects.get(pk=9)
+    # post.comment_set.create(text='im in australia and its very hot here', post=post, author=user_obj)    
+    # post.comment_set.create(text='not rainy, there are some clouds though!', post=post, author=user_obj)
+    # post.comment_set.create(text='raining all day here at india', post=post, author=user_obj)
+
+    # print(post.comment_set.all())
     return render(request, "reddit_app/index.html", {})
+
+def single_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    print(post.text)
+    comments = post.comment_set.all()
+    print(comments)
+    return render(request, "reddit_app/single_post.html", {'post':post, 'comments':comments})
