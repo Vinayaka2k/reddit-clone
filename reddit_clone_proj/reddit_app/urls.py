f23763bcd76204d172a7dabf5aacd64dfcc559d1
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-01-26 13:45:11 +0530

    added code for creating new post

--- reddit_clone_proj/reddit_app/urls.py
+++ reddit_clone_proj/reddit_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "reddit_app"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("create-post/", views.create_post, name="create_post")
 ]
--- reddit_clone_proj/reddit_app/views.py
+++ reddit_clone_proj/reddit_app/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Post, Comment
+from django.contrib.auth.models import User
 
 def index(request):
+    
+    return render(request, "reddit_app/index.html", {})
+
+def create_post(request):
+    # user_obj = User.objects.get(pk=1)
+    # post_obj = Post.objects.create(
+    #                 title="Your Fav Movie?",
+    #                 text="Please comment your favourite movie watched preferably in Action/Drama genre",
+    #                 author = user_obj
+    #             )
+    # print(post_obj.id)
+    # print(Post.objects.all())
     return render(request, "reddit_app/index.html", {})
