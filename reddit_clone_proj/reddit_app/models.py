18bd92cb8cce84291d9740a91913913b16d5294e
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-01-26 23:44:05 +0530

    added new model for votes

--- reddit_clone_proj/reddit_app/models.py
+++ reddit_clone_proj/reddit_app/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+from sqlite3 import IntegrityError
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,8 +10,34 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def upvote(self, user):
+        try:
+            self.vote_set.create(user=user, post=self, vote_type=True)
+            self.votes += 1
+            self.save()
+        except IntegrityError:
+            return 'already_upvoted'
+        return 'ok'
+
+    def downvote(self, user):
+        try:
+            self.vote_set.create(user=user, post=self, vote_type=False)
+            self.votes -= 1
+            self.save()
+        except IntegrityError:
+            return 'already_downvoted'
+        return 'ok'
+
 class Comment(models.Model):
     text = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    vote_type = models.BooleanField()
+
+    class Meta:
+        unique_together = ('user', 'post',)
