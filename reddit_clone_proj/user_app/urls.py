a2f6862bf3c0d8d176a03ef368d761062b16a489
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-01-27 13:10:22 +0530

    login and signup functionality added for user Model

--- reddit_clone_proj/user_app/urls.py
+++ reddit_clone_proj/user_app/urls.py
@@ -5,8 +5,8 @@ app_name = "user_app"
 
 urlpatterns = [
     # path("", views.login, name="login"),
-    # path("signup/", views.signup, name="signup"),
-    # path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
     # path("logout/", views.logout, name="logout"),
     # path("settings/", views.settings, name="settings"),
     # path("delete/", views.delete, name="delete"),
--- reddit_clone_proj/user_app/views.py
+++ reddit_clone_proj/user_app/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth import authenticate, login as sess_login, logout as sess_logout
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 
+def signup(request):
+    context = {}
+    if request.user.is_authenticated:
+        return redirect("reddit_app:index")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+        email = request.POST["email"]
 
-# def signup(request):
-#     return render(request, "user_app/login.html", {})
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username, email, password)
+                sess_login(request, user)
+                return redirect("reddit_app:index")
+            except IntegrityError:
+                context = { "error": "Username or email already taken, please try again" }
+        else:
+            context = {"error" : "Passwords dont match, please try again"}
+    return render(request, "user_app/signup.html", context)
 
-# def login(request):
-#     return render(request, "user_app/login.html", {})
+def login(request):
+    context = {}
+    if request.user.is_authenticated:
+        return redirect("reddit_app:index")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            sess_login(request, user)
+            return redirect("reddit_app:index")
+        else:
+            context = {"error" : "Invalid Username and Password Combination"}
+    return render(request, "user_app/login.html", context)
 
 
 # def signin(request):
